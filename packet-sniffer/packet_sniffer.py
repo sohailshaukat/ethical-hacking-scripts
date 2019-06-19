a7495b7b3ed9b47ae858c99042c7d1767880f71f
root <root@localhost.localdomain>
2019-06-19 12:26:03 -0400

    Packet sniffer added

---
+++ packet-sniffer/packet_sniffer.py
@@ -0,0 +1,13 @@
+#1/usr/bin/env python
+
+import scapy.all as scapy
+
+
+def sniff(interface):
+    scapy.sniff(iface = interface, store = False, prn = process_sniffed_packet)
+
+def process_sniffed_packet(packet):
+    print(packet)
+
+if __name__ == "__main__":
+    sniff("wlan0")
