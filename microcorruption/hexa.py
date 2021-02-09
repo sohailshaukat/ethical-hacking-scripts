80ec25a995fbb44c4f6d67064d662bd1c518c45f
Sohail Shaukat Selod <sohail47k@gmail.com>
2021-02-09 10:15:01 +0530

    Update hexa.py

--- microcorruption/hexa.py
+++ microcorruption/hexa.py
@@ -1,4 +1,6 @@
 #!python3
-print(list(map(lambda x : hex(ord(x)), input() )))
+print("".join(map(lambda x : hex(ord(x)).replace("0x",""), input() )))
 
-print(list(map(lambda x : chr(int(x, 16)) , input().split(" "))))
+# print(list(map(lambda x : chr(int(x, 16)) , input().split(" "))))
+
+# -8fBVvp
